a15ee0c264f443e603605c106e406bd7ab2a23e2
Owais Islam <owaisislam@gmail.com>
2021-01-15 16:16:36 -0600

    added Provider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { Provider } from "react-redux";
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -10,8 +11,9 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Success from "./pages/Success";
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
+
+import store from "./utils/store";
 
 const client = new ApolloClient({
 	request: operation => {
@@ -30,7 +32,7 @@ function App() {
 		<ApolloProvider client={client}>
 			<Router>
 				<div>
-					<StoreProvider>
+					<Provider store={store}>
 						<Nav />
 						<Switch>
 							<Route exact path="/" component={Home} />
@@ -41,7 +43,7 @@ function App() {
 							<Route exact path="/success" component={Success} />
 							<Route component={NoMatch} />
 						</Switch>
-					</StoreProvider>
+					</Provider>
 				</div>
 			</Router>
 		</ApolloProvider>
